5d8a9a162f2b791b61e278e131275e1b7f8d1146
oscma01 <110224332+oscma01@users.noreply.github.com>
2022-08-03 15:24:23 +0200

    Initial commit

---
+++ Widget.js
@@ -0,0 +1,81 @@
+///////////////////////////////////////////////////////////////////////////
+// Copyright © Esri. All Rights Reserved.
+//
+// Licensed under the Apache License Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+///////////////////////////////////////////////////////////////////////////
+define(['dojo/_base/declare', 'jimu/BaseWidget'],
+  function(declare, BaseWidget) {
+    //To create a widget, you need to derive from BaseWidget.
+    return declare([BaseWidget], {
+      // Custom widget code goes here
+
+      baseClass: 'jimu-widget-mywidget',
+
+      //this property is set by the framework when widget is loaded.
+      //name: 'CustomWidget',
+
+
+      //methods to communication with app container:
+
+      // postCreate: function() {
+      //   this.inherited(arguments);
+      //   console.log('postCreate');
+      // },
+
+       startup: function() {
+        this.inherited(arguments);
+        this.mapIdNode.innerHTML = 'map id:' + this.map.id;
+		
+        console.log('startup');
+		conolse.log(this.map.id)
+       },
+
+       onOpen: function(){
+		   //https://your.mapspace.com/index.html?workspace=Default_penta&srs=EPSG:3008&zoom=5&y=6408565.071726729&x=196196.8112791335
+		   this.mapExtentNode.innerHTML = 'https://your.mapspace.com/index.html?workspace=Default_penta&srs=EPSG:4326&zoom=5&y=' + (this.map.geographicExtent.ymin + this.map.geographicExtent.ymax)/2 + '&x=' + (this.map.geographicExtent.xmin + this.map.geographicExtent.xmax)/2;
+         console.log('onOpen');
+       },
+
+      // onClose: function(){
+      //   console.log('onClose');
+      // },
+
+      // onMinimize: function(){
+      //   console.log('onMinimize');
+      // },
+
+      // onMaximize: function(){
+      //   console.log('onMaximize');
+      // },
+
+      // onSignIn: function(credential){
+      //   /* jshint unused:false*/
+      //   console.log('onSignIn');
+      // },
+
+      // onSignOut: function(){
+      //   console.log('onSignOut');
+      // }
+
+      // onPositionChange: function(){
+      //   console.log('onPositionChange');
+      // },
+
+      // resize: function(){
+      //   console.log('resize');
+      // }
+
+      //methods to communication between widgets:
+
+    });
+  });
